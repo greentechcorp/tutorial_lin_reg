84da298f295999eaf378384d9ecdb3197b04bbc0
mbkorecki <mbkorecki@gmail.com>
2019-12-02 12:57:52 +0000

    added the presentation and answers

---
+++ demoAns.py
@@ -0,0 +1,42 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+np.random.seed(2)
+
+def rbf_1d(xx, cc, hh = 1):
+    return np.exp(-np.power((xx-cc), 2) / hh**2)
+
+def phi_rbf(xx):
+    temp = np.array([rbf_1d(xx, 1), rbf_1d(xx, 2), rbf_1d(xx, 3), rbf_1d(xx, 4), rbf_1d(xx, 5), rbf_1d(xx, 6)])[:,:,0].T
+    return np.concatenate([np.ones((temp.shape[0],1)), temp], axis=1)
+
+def phi_quad(xx):
+    temp = np.array([np.power(xx, 1), np.power(xx, 2), np.power(xx, 3), np.power(xx, 4)])[:,:,0].T
+    return np.concatenate([np.ones((temp.shape[0],1)), temp], axis=1)
+
+def phi_lin(xx):
+    return np.concatenate([np.ones((xx.shape[0],1)), xx], axis=1)
+
+def fit(phi, X, yy, alpha=1):
+    X_bias = phi(X)
+    X_bias = np.append(X_bias, (np.identity(X_bias.shape[1]) * np.sqrt(alpha)), axis=0)
+    yy = np.concatenate([yy, np.zeros(X_bias.shape[1])], axis=0)
+    w_fit = np.linalg.lstsq(X_bias, yy, rcond=None)[0]
+    return w_fit
+    
+N = 10
+X = np.linspace(0, 10, num=N).reshape(N, 1)
+yy = rbf_1d(X.reshape(N,), 5) + 0.05*np.random.randn(N)
+
+x_grid = np.linspace(0, 10, num=N*N).reshape(N*N, 1)
+yy_true = rbf_1d(x_grid.reshape(N*N,), 5)
+
+plt.clf()
+plt.plot(X, yy, '+b')
+plt.plot(x_grid, yy_true, '-k')
+
+# plt.plot(x_grid, phi_lin(x_grid).dot(fit(phi_lin, X, yy)), '-r')
+# plt.plot(x_grid, phi_quad(x_grid).dot(fit(phi_quad, X, yy)), '-g')
+plt.plot(x_grid, phi_rbf(x_grid).dot(fit(phi_rbf, X, yy)), '-y')
+
+plt.show()
